4b9762e51bd03a01599a7046c60766b326a496dc
sir-grr <grr1206@gmail.com>
2021-02-10 16:49:33 +0000

    cheking deployment ability

--- WebHookResponse.py
+++ WebHookResponse.py
@@ -1,8 +1,19 @@
-from flask import Flask, request, Response
+#from flask import Flask, request, Response
+
+#app = Flask(__name__)
+
+#@app.route('/webhook', methods=['POST'])
+#def respond():
+#    print(request.json)
+#    return Response(status=200)
+
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
-def respond():
-    print(request.json)
-    return Response(status=200)
+@app.route('/')
+def hello_world():
+  return 'Hello from Flask!'
+
+if __name__ == '__main__':
+  app.run()
