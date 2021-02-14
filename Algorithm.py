6a04f8874f0c5c2de725113b05aa2d314e3f3f6b
sir-grr <grr1206@gmail.com>
2021-02-14 18:47:40 +0000

    test currency commit

--- Algorithm.py
+++ Algorithm.py
@@ -17,10 +17,64 @@ client = Client(api_key, api_secret)
 client.API_URL = 'https://testnet.binance.vision/api'
 ## main
 
+
+def checkCurrency(currency):
+	paymentOptions = ["BNB", "BTC", "ETH"]
+	symbol = ""
+	for payment in paymentOptions:
+		if(TestTrade(currency + payment)):
+			symbol = currency + payment
+			break
+	print(symbol)
+
+def TestTrade(paymentMethod):
+	try:
+		checkTrade = client.get_symbol_ticker(symbol=paymentMethod)
+	except BinanceAPIException as e:
+		print(e)
+		return False
+	return True
+
+def beginTrading(tradeSymbol, targetedCurrencySymbol, originCurrencySymbol):
+	buyInPrice = 0
+	originCurrencyAvailable = client.get_asset_balance(asset=originCurrencySymbol)['free']
+	originCurrencyAvailable = float(originCurrencyAvailable)*0.45
+	i = 0
+	while (client.get_asset_balance(asset=targetedCurrencySymbol) is type(None)):
+		if(i>0):
+			print('allo')
+		buyInPrice = client.get_ticker(symbol=tradeSymbol)['askPrice']
+		try:
+			
+   			buy_order = client.order_limit_buy(
+				symbol=tradeSymbol,
+				quantity=originCurrencyAvailable/buyInPrice,
+				price=buyInPrice,
+			)
+			
+
+		except BinanceAPIException as e:
+			print(e)
+		except BinanceOrderException as e:
+			print(e)
+
+		time.sleep(1)
+		i += 1
+		print(buy_order['orderId'])
+
+
+beginTrading("EOSETH","EOS","ETH")
+
+
 # get latest price from Binance API
+#ETHGBP_price = 0
+#try:
+#	ETHGBP_price = client.get_symbol_ticker(symbol='GARBUTP')
+#except BinanceAPIException as e:
+	# error handling goes here
+#	print(e)
 
-
-#ETHGBP_price = client.get_symbol_ticker(symbol='ETHGBP')
+#print(ETHGBP_price)
 # print full output (dictionary)
 # print(ETHUSD_price)
 #print(ETHBTC_price)
@@ -35,29 +89,32 @@ client.API_URL = 'https://testnet.binance.vision/api'
 # make a test order first. This will raise an exception if the order is incorrect.
 #while True:
 	#print('Sell Price')
+	#HOW MUCH YOU GET FOR SELLING
 	#print(client.get_symbol_ticker(symbol='ETHBTC')['price'])
 	#print()
 	#time.sleep(1)
 	#print('Buy Price')
+	#HOW MUCH YOU NEED TO PAY TO BUY
 	#print(client.get_ticker(symbol='ETHBTC')['askPrice'])
 
-print(client.get_ticker(symbol='ETHBTC'))	
+#print(client.get_ticker(symbol='ETHBTC'))	
 
-try:
+#try:
 	#time.sleep(10)
-	print('howdy')
+#	print('howdy')
    #buy_order = client.order_market_sell(
 	#	symbol='ETHBTC',
 	#	quantity=1
 	#)
 
-except BinanceAPIException as e:
+#except BinanceAPIException as e:
 	# error handling goes here
-	print(e)
-except BinanceOrderException as e:
+#	print(e)
+#except BinanceOrderException as e:
 	# error handling goes here
-	print(e)
+#	print(e)
 
 #print('after buying balance of btc:',client.get_asset_balance(asset='BTC'))
 #print('after buying balance of eth:',client.get_asset_balance(asset='ETH'))
-#print(client.get_account())
+#print(client.get_asset_balance(asset='EOS')['free'])
+print(client.get_account())
