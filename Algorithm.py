61acd327da5cd30fefadbe1eefcd45b5635b3f94
sir-grr <grr1206@gmail.com>
2021-03-16 22:45:49 +0000

    buying and selling independently commit

--- Algorithm.py
+++ Algorithm.py
@@ -44,15 +44,25 @@ def testTrade(paymentMethod):
 def beginTrading(tradeSymbol, targetedCurrencySymbol, originCurrencySymbol):
 	buyInPrice = 0
 	purchaseQuantity = 0
+	orderId = ''
 	originCurrencyAvailable = client.get_asset_balance(asset=originCurrencySymbol)['free']
 	originCurrencyAvailable = Decimal(originCurrencyAvailable) * Decimal(0.9)
 	i = 0
 	precision = int(client.get_symbol_info(symbol=tradeSymbol)['baseAssetPrecision'])
 	stepSize = Decimal(client.get_symbol_info(symbol=tradeSymbol)['filters'][2]['stepSize'])
 	
-	while (i == 0):
+	while ((Decimal(client.get_asset_balance(asset=targetedCurrencySymbol)['free']) <= 0) or (i > 2)):
 		if(i>0):
-			print('allo')
+			try:
+				print("canceling",orderId)
+				result = client.cancel_order(
+				symbol= tradeSymbol,
+				orderId= orderId)
+
+			except BinanceAPIException as e:
+				print(e)
+			except BinanceOrderException as e:
+				print(e)
 		
 		try:
 			buyInPrice = Decimal(client.get_ticker(symbol=tradeSymbol)['askPrice'])
@@ -60,15 +70,15 @@ def beginTrading(tradeSymbol, targetedCurrencySymbol, originCurrencySymbol):
 			availableSteps = math.trunc(purchaseQuantity/stepSize)
 			purchaseQuantity = availableSteps * stepSize
 			purchaseQuantity = round(purchaseQuantity,precision)
-			#print(purchaseQuantity*buyInPrice)
-			print(buyInPrice)
+			print("price buying at",buyInPrice)
 			print("amount buying",purchaseQuantity)
 			buy_order = client.order_limit_buy(
 			symbol=tradeSymbol,
 			quantity=purchaseQuantity,
 			price=round(buyInPrice,precision),
 			)
-			print(buy_order['orderId'])
+
+			orderId = buy_order['orderId']
 		
 
 		except BinanceAPIException as e:
@@ -76,41 +86,53 @@ def beginTrading(tradeSymbol, targetedCurrencySymbol, originCurrencySymbol):
 		except BinanceOrderException as e:
 			print(e)
 
-		time.sleep(1)
+		time.sleep(2)
 		i += 1
-		print(buyInPrice)
 	
-	time.sleep(45)
-	endTrading(tradeSymbol,buyInPrice, purchaseQuantity)
+	if(i <= 2):
+		print("BOUGHT")
+		time.sleep(30)
+		endTrading(tradeSymbol,buyInPrice, targetedCurrencySymbol)
 
-def endTrading(tradeSymbol, buyInPrice, purchaseQuantity):
-	i = 0
+def endTrading(tradeSymbol, buyInPrice, targetedCurrencySymbol):
 	sellPrice = 0
+	orderMade = False
 	orderId = ''
-	precision = int(client.get_symbol_info(symbol=tradeSymbol)['baseAssetPrecision'])
-	#stepSize = Decimal(client.get_symbol_info(symbol=tradeSymbol)['filters'][2]['stepSize'])
-	
-	while (i == 0):
-		if(i>0):
-			result = client.cancel_order(
-    		symbol= tradeSymbol,
-    		orderId= orderId)
-		
+	quantityOwned = Decimal(client.get_asset_balance(asset=targetedCurrencySymbol)['free'])
+	precision = int(client.get_symbol_info(symbol=tradeSymbol)['baseAssetPrecision'])	
+	while ((quantityOwned > Decimal(0)) or(Decimal(client.get_asset_balance(asset=targetedCurrencySymbol)['locked']) > 0)):
+		if(orderMade):
+			try:
+				print("canceling",orderId)
+				result = client.cancel_order(
+				symbol= tradeSymbol,
+				orderId= orderId)
+				orderMade = False
+
+			except BinanceAPIException as e:
+				print(e)
+			except BinanceOrderException as e:
+				print(e)
+
+		quantityOwned = Decimal(client.get_asset_balance(asset=targetedCurrencySymbol)['free'])
 		try:
-			sellPrice = Decimal(client.get_symbol_ticker(symbol=tradeSymbol)['price'])
-			sellPrice = round(sellPrice,precision)
-			if(sellPrice > buyInPrice):
-				#print(purchaseQuantity*buyInPrice)
-				print(sellPrice)
-				print("amount selling",purchaseQuantity)
-				sell_order = client.order_limit_sell(
-				symbol=tradeSymbol,
-				quantity=purchaseQuantity,
-				price=sellPrice,
-				)
 			
-				print(sell_order['orderId'])
-				orderId = sell_order['orderId']
+			while(orderMade == False):
+				sellPrice = Decimal(client.get_symbol_ticker(symbol=tradeSymbol)['price'])
+				sellPrice = round(sellPrice,precision)
+				print("testing Price",sellPrice)
+				time.sleep(1)
+				if(sellPrice > Decimal(buyInPrice) * Decimal(1.002004)):
+					print("price selling at",sellPrice)
+					print("amount selling",quantityOwned)
+					sell_order = client.order_limit_sell(
+					symbol=tradeSymbol,
+					quantity=quantityOwned,
+					price=sellPrice,
+					)
+					orderMade = True
+			
+					orderId = sell_order['orderId']
 		
 
 		except BinanceAPIException as e:
@@ -118,14 +140,14 @@ def endTrading(tradeSymbol, buyInPrice, purchaseQuantity):
 		except BinanceOrderException as e:
 			print(e)
 
-		time.sleep(1)
-		i += 1
-		print(sellPrice)
+		
+		time.sleep(2)
+	print("SOLD")
 
 
-#checkCurrency("WAN")
-#beginTrading("AVAXBNB","AVAX","BNB")
-endTrading("WANBNB",0.00488,19.0)
+checkCurrency("ADA")
+#beginTrading("BIFIBNB","BIFI","BNB")
+#endTrading("IQBNB",0.00008630,"IQ")
 
 #minor testing for the get _symbol_ticker(part of development)
 #print(Decimal(client.get_symbol_ticker(symbol="OGNBNB")['price']))
